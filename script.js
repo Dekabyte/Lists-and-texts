7ba06884966361832964dbadea8610fe3be0c3f2
Dekabyte <wdb126036@gmail.com>
2020-04-12 08:18:09 +0200

    Minden kész

--- script.js
+++ script.js
@@ -2,15 +2,30 @@ $('body').append(
 '<h1>Hello Wohhhhhhhhhhhhhhhhhhhhhhhhrld!</h1>'
 );
 
-let tomb = ['Laci', 'Viki', 'Andris', 'Vivien'];
+let tomb = ['Pisti', 'Laci', 'Viki', 'Andris', 'Vivien'];
 
-tomb.forEach(nev => $( 'ul' ).append( '<li>' + nev + '</li>' ))
+// Ez a jó, de még bold nélkül: tomb.forEach(nev => $( 'ul' ).append( '<li>' + nev + '</li>' ))
 
-// if (nev = 'Laci') { nev = '<b>' + nev + '</b>' };
+tomb.forEach(nev => {
+if (nev != 'Laci') {
+    $( 'ul' ).append( '<li>' + nev + '</li>' );
+        } else {
+    $( 'ul' ).append( '<li>' + '<b>' + nev + '</b>' + '</li>' );
+}});
+
+
+// Teljes megoldás, de for ciklussal:
+//for (let I = 0; I < tomb.length; I++) {
+//    if (tomb[I] != 'Laci') {
+//        $( 'ul' ).append( '<li>' + tomb[I] + '</li>' );
+//         } else {
+//        $( 'ul' ).append( '<li>' + '<b>' + tomb[I] + '</b>' + '</li>' );
+//    }
+//}
 
 let additionalBlock = {
-    title: "Added with javascript",
-    text: "This block was added using JavaScript's jQuery library. How awesome!"
+title: "Added with javascript",
+text: "This block was added using JavaScript's jQuery library. How awesome!"
 };
 
 $( 'h1' ).append( additionalBlock.title );
